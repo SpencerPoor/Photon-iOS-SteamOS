9d98bb5101e04defa022595c7b30fe2bbe625c67
Spencer Poor <speneth1@gmail.com>
2025-01-08 18:44:49 +0900

    librarysync: Fleshed out image preview of permitted media

--- ios-root/app/(tabs)/_layout.tsx
+++ ios-root/app/(tabs)/_layout.tsx
@@ -1,96 +1,104 @@
 import { Tabs } from "expo-router";
 import Ionicons from '@expo/vector-icons/Ionicons';
 import React from "react";
-import { Easing } from "react-native";
+import { Easing, View, StyleSheet } from "react-native";
 
 export default function RootLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        animation: 'fade',
-        tabBarActiveTintColor: 'white',
-        headerStyle: {
-          backgroundColor: '#25292e',
-        },
-        headerShadowVisible: false,
-        headerTintColor: '#fff',
-        tabBarStyle: {
-          backgroundColor: '#25292e',
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          transitionSpec: {
-            animation: 'timing',
-            config: {
-              duration: 150,
-              easing: Easing.inOut(Easing.ease),
-            },
+    <View style={styles.tabsContainer}>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: 'white',
+          headerStyle: {
+            backgroundColor: '#25292e',
           },
-          sceneStyleInterpolator: ({ current }) => ({
-            sceneStyle: {
-              opacity: current.progress.interpolate({
-                inputRange: [-1, 0, 1],
-                outputRange: [0, 1, 0],
-              }),
-            },
-          }),
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="librarysync"
-        options={{
-          title: 'Library Sync',
-          transitionSpec: {
-            animation: 'timing',
-            config: {
-              duration: 150,
-              easing: Easing.inOut(Easing.ease),
-            },
+          headerShadowVisible: false,
+          headerTintColor: '#fff',
+          tabBarStyle: {
+            backgroundColor: '#25292e',
           },
-          sceneStyleInterpolator: ({ current }) => ({
-            sceneStyle: {
-              opacity: current.progress.interpolate({
-                inputRange: [-1, 0, 1],
-                outputRange: [0, 1, 0],
-              }),
-            },
-          }),
-          tabBarIcon: ({color, focused }) => (
-            <Ionicons name={focused ? 'sync' : 'sync-outline'} color={color} size={24}/>
-          ),
         }}
-      />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: 'Settings',
-          transitionSpec: {
-            animation: 'timing',
-            config: {
-              duration: 150,
-              easing: Easing.inOut(Easing.ease),
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Home',
+            transitionSpec: {
+              animation: 'timing',
+              config: {
+                duration: 100,
+                easing: Easing.inOut(Easing.ease),
+              },
             },
-          },
-          sceneStyleInterpolator: ({ current }) => ({
-            sceneStyle: {
-              opacity: current.progress.interpolate({
-                inputRange: [-1, 0, 1],
-                outputRange: [0, 1, 0],
-              }),
+            sceneStyleInterpolator: ({ current }) => ({
+              sceneStyle: {
+                opacity: current.progress.interpolate({
+                  inputRange: [-1, 0, 1],
+                  outputRange: [0, 1, 0],
+                }),
+              },
+            }),
+            tabBarIcon: ({ color, focused }) => (
+              <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="librarysync"
+          options={{
+            title: 'Library Sync',
+            transitionSpec: {
+              animation: 'timing',
+              config: {
+                duration: 100,
+                easing: Easing.inOut(Easing.ease),
+              },
             },
-          }),
-          tabBarIcon: ({color, focused }) => (
-            <Ionicons name={focused ? 'settings' : 'settings-outline'} color={color} size={24}/>
-          ),
-        }}
-      />
-    </Tabs>
+            sceneStyleInterpolator: ({ current }) => ({
+              sceneStyle: {
+                opacity: current.progress.interpolate({
+                  inputRange: [-1, 0, 1],
+                  outputRange: [0, 1, 0],
+                }),
+              },
+            }),
+            tabBarIcon: ({color, focused }) => (
+              <Ionicons name={focused ? 'sync' : 'sync-outline'} color={color} size={24}/>
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="settings"
+          options={{
+            title: 'Settings',
+            transitionSpec: {
+              animation: 'timing',
+              config: {
+                duration: 100,
+                easing: Easing.inOut(Easing.ease),
+              },
+            },
+            sceneStyleInterpolator: ({ current }) => ({
+              sceneStyle: {
+                opacity: current.progress.interpolate({
+                  inputRange: [-1, 0, 1],
+                  outputRange: [0, 1, 0],
+                }),
+              },
+            }),
+            tabBarIcon: ({color, focused }) => (
+              <Ionicons name={focused ? 'settings' : 'settings-outline'} color={color} size={24}/>
+            ),
+          }}
+        />
+      </Tabs>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  tabsContainer: {
+    flex: 1,
+    backgroundColor: '#25292e',
+  },
+});
--- ios-root/app/(tabs)/librarysync.tsx
+++ ios-root/app/(tabs)/librarysync.tsx
@@ -1,21 +1,90 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, FlatList, Image, Pressable, Dimensions } from "react-native";
+import { useState, useEffect } from 'react';
+import * as MediaLibrary from "expo-media-library";
+
+// Device screen width to reference for photo rendering
+const screenWidth = Dimensions.get('window').width;
+
+type Photo = {
+  id: string;
+  uri: string;
+  isSelected: boolean;
+}
 
 export default function LibrarySync() {
+  // Array of photos permitted to be accessed
+  const [photos, setPhotos] = useState<Photo[]>([])
+
+  // On page load, loads photos for preview
+  useEffect(() => {
+    loadPhotos();
+  }, []);
+
+  // Loads photos from device media library
+  const loadPhotos = async () => {
+    const { assets } = await MediaLibrary.getAssetsAsync({
+      mediaType: MediaLibrary.MediaType.photo,
+      first: 50, // Fetch the first 50 photos
+    });
+
+    // Maps necessary metadata from each photo to an array for rendering on screen
+    const formattedPhotos = assets.map((photo) => ({
+      id: photo.id,
+      uri: photo.uri,
+      isSelected: false, // Initially, no photo is selected
+    }));
+
+    setPhotos(formattedPhotos);
+  };
+
+  // Toggles if the photo is selected for syncing
+  const togglePhotoSelection = (id: string) => {
+    setPhotos((prevPhotos) =>
+      prevPhotos.map((photo) =>
+        photo.id === id ? { ...photo, isSelected: !photo.isSelected } : photo
+      )
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Library Sync Screen</Text>
-    </View>
+    // Renders photos into an interactable FlatList
+    <FlatList
+      data={photos}
+      keyExtractor={(item) => item.id}
+      numColumns={3}
+      renderItem={({ item }) => (
+        <Pressable onPress={() => togglePhotoSelection(item.id)}>
+          <View style={[styles.photoContainer, item.isSelected && styles.selectedPhotoContainer]}>
+            <Image source={{ uri: item.uri }} style={[ styles.photo ]}/>
+          </View>
+        </Pressable>
+      )}
+      contentContainerStyle={styles.gallery}
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#25292e',
-    justifyContent: 'center',
-    alignItems: 'center',
+  gallery: {
+    paddingTop: 10,
+    backgroundColor: "#25292e",
     flex: 1,
   },
-  text: {
-    color: 'white',
-  }
-})
+  photoContainer: {
+    width: screenWidth / 3,
+    height: screenWidth / 3,
+    borderWidth: 0.5,
+    borderColor: 'black',
+  },
+  selectedPhotoContainer: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Dim the photo with a semi-transparent overlay
+  },
+  photo: {
+    width: '100%',
+    height: '100%',
+  },
+  selectedPhoto: {
+    borderWidth: 3,
+    borderColor: "green",
+  },
+});
